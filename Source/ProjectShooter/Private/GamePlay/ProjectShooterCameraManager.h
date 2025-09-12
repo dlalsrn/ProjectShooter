7784d40bf89aaefad2e27deedbc22ee80870e305
LEE MINGU <qazzaq1541@gmail.com>
2025-09-12 16:00:13 +0900

    Remove source code from ThirdPerson template, and create cpp class for ProjectShooter

---
+++ Source/ProjectShooter/Private/GamePlay/ProjectShooterCameraManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/PlayerCameraManager.h"
+#include "ProjectShooterCameraManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AProjectShooterCameraManager : public APlayerCameraManager
+{
+	GENERATED_BODY()
+	
+};
