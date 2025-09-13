8b1ee95c374773ec3ca1ebdf120f29da57744bb4
LEE MINGU <qazzaq1541@gmail.com>
2025-09-13 21:04:47 +0900

    Migrate BP_Bullet into cpp

--- Source/ProjectShooter/Private/Weapon/ProjectShooterBullet.cpp
+++ Source/ProjectShooter/Private/Weapon/ProjectShooterBullet.cpp
@@ -9,6 +9,11 @@ AProjectShooterBullet::AProjectShooterBullet()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	BulletCpp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BulletCpp"));
+	if (BulletCpp)
+	{
+		RootComponent = BulletCpp;
+	}
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +21,10 @@ void AProjectShooterBullet::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (BulletCpp)
+	{
+		BulletCpp->OnComponentHit.AddUniqueDynamic(this, &AProjectShooterBullet::OnHitCallback);
+	}
 }
 
 // Called every frame
@@ -25,3 +34,7 @@ void AProjectShooterBullet::Tick(float DeltaTime)
 
 }
 
+void AProjectShooterBullet::OnHitCallback(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	Destroy();
+}
--- Source/ProjectShooter/Private/Weapon/ProjectShooterBullet.h
+++ Source/ProjectShooter/Private/Weapon/ProjectShooterBullet.h
@@ -19,8 +19,14 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
+	void OnHitCallback(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> BulletCpp;
 };
